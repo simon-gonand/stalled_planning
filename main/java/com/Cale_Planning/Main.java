53561a42d91393d6980edc2b9b856dfcc7379193
simon-gonand <simon.gonand@etu.univ-amu.fr>
2020-05-22 11:53:46 +0200

    create main window + set icon

--- main/java/com/Cale_Planning/Main.java
+++ main/java/com/Cale_Planning/Main.java
@@ -1,33 +1,16 @@
 package com.Cale_Planning;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import com.Cale_Planning.Models.Adherent;
+
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args){
-        MSAccessBase database = new MSAccessBase("src/main/resources/Database.accdb");
-        database.connect();
-        try {
-            ResultSet resultSet = database.SQLSelect("SELECT * FROM Adherent");
-            while (resultSet.next()){
-                System.out.println("---------------- Adherent n° " + resultSet.getInt("ID") + " ----------------");
-                System.out.println(resultSet.getString("Genre") + " " + resultSet.getString("Prenom") + " " +
-                        resultSet.getString("Nom"));
-                System.out.println("Né le " + resultSet.getDate("Date_Naissance").toString());
-                System.out.println("Adresse : " + resultSet.getString("Rue") + " " + resultSet.getInt("CodePostal") +
-                        " " + resultSet.getString("Ville"));
-                System.out.println("Email : " + resultSet.getString("Email"));
-                System.out.println("Téléphone : " + resultSet.getString("Telephone"));
-                System.out.println("Portable : " + resultSet.getString("Portable"));
-                System.out.println("Admis en : " + resultSet.getString("Date_adhesion"));
-                System.out.println("Commentaire : " + resultSet.getString("Com"));
-                ResultSet bateauResult = database.SQLSelect("SELECT Nom FROM Bateau WHERE Proprietaire = " + resultSet.getString("ID"));
-                bateauResult.next();
-                System.out.println("Bateau : " + bateauResult.getString("Nom"));
-                System.out.println();
-            }
-        } catch (SQLException e){
-            System.out.println("Query exception n° " + e.getErrorCode() + " Message: " + e.getMessage());
-        }
+        JFrame mainWindow = new JFrame("Planning Cale");
+        ImageIcon icon = new ImageIcon("src/main/resources/Main_Icon.png");
+        mainWindow.setIconImage(icon.getImage());
+        mainWindow.setVisible(true);
+        mainWindow.setExtendedState(JFrame.MAXIMIZED_BOTH); // Set fullscreen window
+        mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
